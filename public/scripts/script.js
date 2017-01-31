8034dee515886fb70653569719f871222cb727ef
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2017-01-31 14:16:52 -0500

    added Jquery to top buttons

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function() {
+   $('button').mouseenter(function() {
+       $(this).animate({
+           height: '+=10px'
+       });
+   });
+   $('button').mouseleave(function() {
+       $(this).animate({
+           height: '-=10px'
+       });
+   });
+   $('button').click(function() {
+       $(this).toggle(1000);
+   });
+   $('.btn-success').on("Click", function(){
+     $('#first-container').html("<h1>What have you achieved today ?<h1>");
+   });
 
 
-
-    })
+});
