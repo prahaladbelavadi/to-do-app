29b7262fb831f41fbf36c1743cf70712f4f697f0
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2017-02-01 15:05:28 -0500

    remaining characters not working, div removal and entry not working,Jquery problem

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -11,21 +11,24 @@ $(document).ready(function() {
    });
    $('button').click(function() {
        $(this).toggle(1000);
-   });
-   $(".success").hover(function(){
-    alert("You entered p1!");
-},
-function(){
-    alert("Bye! You now leave p1!");
-});
+//    });
+//    $(".success").hover(function(){
+//     alert("You entered p1!");
+// },
+// function(){
+//     alert("Bye! You now leave p1!");
+// });
+//bind input character limit to variable to display number of characters that can be entered
+  var su = success-input.length
+    $("p").append(140-su);
+  });
 
 
-
-   $('.btn-success').on("Click", function(){
-     $('.success').click(function(){
-       $('.hidden').addClass('visible');
-       $('.hidden').removeClass('hidden');
-     })
+  //  $('.btn-success').on("Click", function(){
+  //    $('.success').click(function(){
+  //      $('.hidden').addClass('visible');
+  //      $('.hidden').removeClass('hidden');
+  //    })
    // Not Working    //  $('#first-container').html("<h1>What have you achieved today ?<h1>");
    });
 
